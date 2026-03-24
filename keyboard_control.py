be2590b1002c747bf424461a3f350d7a289c2dfb
mlipsman <meaganlipsman@gmail.com>
2026-03-24 16:43:18 -0400

    test attempt 2 for trying to drive continously from keys

--- keyboard_control.py
+++ keyboard_control.py
@@ -38,33 +38,33 @@ def on_press(key, motor):
         print(f"Error: {e}")
 
 def continuous_forward(motor):
-    print("continuou forward")
-    motor.set_duty_cycle(0.1)
+    print("continous forward")
+    #motor.set_duty_cycle(-0.1)
+
+def continuous_backward(motor):
+    print("continous backward")
+    #motor.set_duty_cycle(0.1)
 
 def continuous_press(key, motor):
-    while key == keyboard.Key.right:
-        print("continuous forward test")
-
-    while key == keyboard.Key.left:
-        print("continuous back test")
-
-            #continuous_forward(motor)
-        #else while key == keyboard.Key.left:
-            #drive_backward(motor)
-        #elif key == keyboard.Key.space:
-            #motor.set_duty_cycle(0)
-    #     elif key == keyboard.Key.esc:
-    #         # Stop the listener
-    #         return False
-    # except Exception as e:
-    #     print(f"Error: {e}")
+    try:
+        if key == keyboard.Key.right:
+            continuous_forward(motor)
+        elif key == keyboard.Key.left:
+            continuous_backward(motor)
+        elif key == keyboard.Key.space:
+            motor.set_duty_cycle(0)
+        elif key == keyboard.Key.esc:
+            # Stop the listener
+            return False
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     with VESC(serial_port=serial_port) as motor:
         print("Control active. Use Arrow Keys to drive, Space to stop, Esc to quit.")
         
         # Use a listener to monitor key presses
-        with keyboard.Listener(on_press=lambda k: continuous_press(k, motor)) as listener:
+        with keyboard.Listener(on_press=lambda k: on_press(k, motor)) as listener:
             listener.join()
 
 
