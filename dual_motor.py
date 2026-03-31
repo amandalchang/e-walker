3f8515aaafa914ea1328b1ffd182b53781f6449f
mlipsman <meaganlipsman@gmail.com>
2026-03-30 23:50:26 -0400

    trying again to encode message

--- dual_motor.py
+++ dual_motor.py
@@ -23,19 +23,50 @@ if __name__ == '__main__':
 
         # # Control Motor 2 (Remote/Slave via CAN)
         # # We create a message and specify the target CAN ID
-        msg_motor_2 = pyvesc.SetDutyCycle(duty_cycle=0.2)
+        msg_motor_2 = SetDutyCycle(duty_cycle=0.1)
         msg_motor_2.can_id = 45  # The ID of your second motor
         # time.sleep(duration)
         #msg_motor_2 = SetDutyCycle(duty_cycle=0)
     
         # # Send the encoded packet through the master
-        pyvesc.encode(msg_motor_2)
+        main.write(main.encode(msg_motor_2))
 
 
-#         # make a SetDutyCycle message
-# my_msg = pyvesc.SetDutyCycle(1e5)
-# print(my_msg.duty_cycle) # prints value of my_msg.duty_cycle
-# my_packet = pyvesc.encode(my_msg)
-# # my_packet (type: bytes) can now be sent over your UART connection
 
+# import serial
+# import time
+# from pyvesc import VESC
+# from pyvesc.VESC.messages.setters import SetDutyCycle
 
+# # 1. Setup the connection
+# # Make sure this matches the port you found earlier!
+# serial_port = '/dev/ttyACM0' 
+
+# try:
+#     with VESC(serial_port=serial_port) as master:
+#         print("Connected to Master VESC")
+
+#         # --- CONTROL MOTOR 1 (Local) ---
+#         # The library has a built-in helper for the local motor
+#         master.set_duty_cycle(0.1) 
+        
+#         # --- CONTROL MOTOR 2 (Over CAN) ---
+#         # For the second motor, we create the message manually
+#         # and assign the CAN ID (Usually 2, if you set it in VESC Tool)
+#         msg_motor_2 = SetDutyCycle(duty_cycle=0.1)
+#         msg_motor_2.can_id = 2 
+        
+#         # We tell the Master to encode and send this over the wire
+#         master.write(master.encode(msg_motor_2))
+        
+#         print("Commands sent to both motors.")
+#         time.sleep(2)
+        
+#         # Stop both
+#         master.set_duty_cycle(0)
+#         stop_m2 = SetDutyCycle(duty_cycle=0)
+#         stop_m2.can_id = 2
+#         master.write(master.encode(stop_m2))
+
+# except Exception as e:
+#     print(f"Error: {e}")
