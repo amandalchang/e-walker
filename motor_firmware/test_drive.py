4317e7059f11fc63ed3ab831efaab09a9f9b2e68
mlipsman <meaganlipsman@gmail.com>
2026-04-28 16:29:53 -0400

    renaming motor firmware directory

---
+++ motor_firmware/test_drive.py
@@ -0,0 +1,23 @@
+from pyvesc import VESC
+import serial
+import time
+
+serial_port = '/dev/ttyACM0' #fix with actual serial port once tested on raspi 
+
+
+## FROM EXAMPLE CODE FROM PYVESC GITHUB DOCUMENTATION
+def run_motor_using_with():
+    with VESC(serial_port=serial_port) as motor:
+        print("Firmware: ", motor.get_firmware_version())
+        motor.set_duty_cycle(.02)
+
+        # run motor and print out rpm for ~2 seconds
+        for i in range(60):
+            time.sleep(0.1)
+            print("here")
+        motor.set_rpm(0)
+
+
+
+if __name__ == '__main__':
+    run_motor_using_with()
